d04200edfba9a86aca14a985e030d0a9227a8653
ReebaPaysys <reeba.siddiqui@paysyslabs.com>
2026-05-21 18:17:08 +0500

    Update src/rule.ts

---
+++ src/rule.ts
@@ -0,0 +1,29 @@
+import type { DatabaseManagerInstance, LoggerService, ManagerConfig } from '@tazama-lf/frms-coe-lib';
+  import type { Case, RuleConfig, RuleRequest, RuleResult } from '@tazama-lf/frms-coe-lib/lib/interfaces';
+import type { SupportedTransactionMessage } from '@tazama-lf/frms-coe-lib/lib/interfaces';
+import type { BaseMessage } from '@tazama-lf/frms-coe-lib/lib/interfaces';
+
+export type RuleExecutorConfig = Required<Pick<ManagerConfig, 'rawHistory' | 'eventHistory' | 'configuration' | 'localCacheConfig'>>;
+
+export async function handleTransaction(
+  req: RuleRequest<SupportedTransactionMessage>,
+  determineOutcome: (value: number, ruleConfig: RuleConfig, ruleResult: RuleResult) => RuleResult,
+  ruleRes: RuleResult,
+  loggerService: LoggerService,
+  ruleConfig: RuleConfig,
+  databaseManager: DatabaseManagerInstance<RuleExecutorConfig>,
+): Promise<RuleResult> {
+  const transaction = req.transaction as BaseMessage;
+  
+  if (!ruleConfig.config.bands) {
+    throw new Error('Invalid config provided - bands not provided');
+  }
+  if (!ruleConfig.config.exitConditions) {
+    throw new Error('Invalid config provided - exitConditions not provided');
+  }
+
+const amount =  req.transaction.Payload.Payload.amount as unknown as number
+
+  return determineOutcome(amount, ruleConfig, ruleRes);
+  
+}
